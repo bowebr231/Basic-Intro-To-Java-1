547f51f7396e01d35b0a1ac305dc28babdc2483c
Bradley Bowen <bowebr231@gmail.com>
2023-02-04 18:16:56 -0600

    First commit. Intellij project setup. Includes Hello World program, and unit test directory

---
+++ src/main/test/Practice/LessonsForJoshTest.java
@@ -0,0 +1,15 @@
+package Practice;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LessonsForJoshTest {
+
+    @Test
+    void mainTest() {
+        // TODO: create function tests
+        assertTrue(true); // defaults to pass for now
+    }
+
+}
